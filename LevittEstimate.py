a6e3451da507691544ad8ecb46ef28b3741336b4
maestro-rayo <71044051+maestro-rayo@users.noreply.github.com>
2020-09-16 22:11:23 -0500

    Export of notebook

--- LevittEstimate.py
+++ LevittEstimate.py
@@ -9,12 +9,13 @@
 # 
 # 
 
-# In[47]:
+# In[96]:
 
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 from math import log
 from datetime import datetime, timedelta
 
@@ -22,37 +23,41 @@ from datetime import datetime, timedelta
 # 
 # ### Get data
 
-# In[48]:
+# In[128]:
 
 
 covid_data = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/total_deaths.csv", parse_dates=["date"], skiprows=[1])
 
 
-# In[49]:
+# In[129]:
 
 
 location = 'Sweden'
 data = covid_data[location].fillna(0).astype(int)
 #data = data.rolling(window=7).mean().fillna(0).astype(int)
+plt.rcParams["figure.figsize"] = (10,6)
 plt.plot(data)
 today = datetime.today()
 print("Max value to date %s for %s is %d" % (today.strftime("%m/%d/%Y"),location,max(data)))
 
 
-# In[50]:
+# In[144]:
 
 
 threshold = 0.99
 
 #Assuming one would get a reasonable estimate at least at a third of the final value
-max_times = 3
+max_times = 2
 
 def ML(data, days_ago):
   data = data[:len(data)-days_ago]
   max_search = max_times*max(data)
   coeff = []
+
+  valid_data = data[data > 0]
+  coeff = []
   for N in range(max(data)+1,max(data) + max_search):
-    y = np.array([-np.log(np.log(N) - np.log(x)) for x in data if x > 0])
+    y = -np.log(np.log(N) - np.log(valid_data.values))
     days = np.array(range(len(y)))
     correlation = np.corrcoef(days, y)[0][1]
     coeff.append(correlation)
@@ -62,19 +67,29 @@ def ML(data, days_ago):
   return (N, max(coeff))
 
 
-# In[55]:
+# In[145]:
 
 
 #What was the prediction X days ago?
-days_ago = 60
-d = today - timedelta(days=days_ago)
-prediction, max_coeff = ML(data,days_ago)
 plt.plot(data,label='Accumulated')
-plt.rcParams["figure.figsize"] = (12,10)
-plt.plot([prediction]*len(data),label='Prediction')
+
+days_ago = 101
+
+for i in range(days_ago,0,-10):
+    d = today - timedelta(days=i)
+    prediction, max_coeff = ML(data,i)
+    #plt.plot([prediction]*len(data),label='Prediction')
+    plt.plot(len(data)-i,prediction,marker='s', color='r')
+
 plt.legend(loc="lower right")
 
 print("For %s the prediction on %s was %d.\nMaximum correlation %f" % (location,d.strftime("%m/%d/%Y"),prediction,max_coeff))
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
